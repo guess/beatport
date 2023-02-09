eb18d1e9d0710605eade542671a2f6ff8cf5becd
Steve Strates <steve@strates.io>
2023-02-08 22:58:47 -0500

    Initial commit

---
+++ lib/beatport.ex
@@ -0,0 +1,18 @@
+defmodule Beatport do
+  @moduledoc """
+  Documentation for `Beatport`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Beatport.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/beatport_test.exs
@@ -0,0 +1,8 @@
+defmodule BeatportTest do
+  use ExUnit.Case
+  doctest Beatport
+
+  test "greets the world" do
+    assert Beatport.hello() == :world
+  end
+end
